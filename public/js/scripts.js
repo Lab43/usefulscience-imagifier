0d7826e604be5560f2d247cbd6bf16c473b233c8
Cameron Spencer <cs@eusonic.com>
2014-03-21 15:08:12 -0700

    improve image styling

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -13,4 +13,16 @@
     $('.text').text(text);
   });
 
+  $('#update_cat').click(function (e) {
+    e.preventDefault();
+    var cat = $('#cat').val();
+    $('[data-cat]').attr('data-cat', cat);
+  });
+
+  $('#update_pos').click(function (e) {
+    e.preventDefault();
+    var pos = $('#pos').val();
+    $('[data-pos]').attr('data-pos', pos);
+  });
+
 }(jQuery));
