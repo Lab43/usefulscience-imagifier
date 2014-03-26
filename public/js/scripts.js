091f60fdf14e8a301eb02c6daa289328126f2095
Cameron Spencer <cs@eusonic.com>
2014-03-25 21:33:21 -0700

    add more style controls

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -13,16 +13,13 @@
     $('.text').text(text);
   });
 
-  $('#update_cat').click(function (e) {
-    e.preventDefault();
-    var cat = $('#cat').val();
-    $('[data-cat]').attr('data-cat', cat);
-  });
-
-  $('#update_pos').click(function (e) {
-    e.preventDefault();
-    var pos = $('#pos').val();
-    $('[data-pos]').attr('data-pos', pos);
+  // update data-??? attributes
+  $.each(['color','cat','pos','back'], function (i, attr) {
+    $('#update_' + attr).click(function (e) {
+      e.preventDefault();
+      var val = $('#' + attr).val();
+      $('[data-' + attr + ']').attr('data-' + attr, val);
+    });
   });
 
 }(jQuery));
