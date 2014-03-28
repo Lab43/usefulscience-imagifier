d01e5470e916290cf55e8add86f342b80d432bef
Cameron Spencer <cs@eusonic.com>
2014-03-28 16:00:04 -0700

    auto-update form

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,25 +1,29 @@
 (function ($, undefined) {
   'use strict';
 
-  $('#update_img').click(function (e) {
-    e.preventDefault();
-    var src = $('#img').val();
+  // update img url
+  $('#img').change(function (e) {
+    var src = $(this).val();
     $('.img').attr('src', src);
   });
 
-  $('#update_text').click(function (e) {
-    e.preventDefault();
-    var text = $('#text').val();
+  // update post text
+  $('#text').on('change keyup paste', function (e) {
+    var text = $(this).val();
     $('.text').text(text);
   });
 
   // update data-??? attributes
   $.each(['color','cat','pos','back'], function (i, attr) {
-    $('#update_' + attr).click(function (e) {
-      e.preventDefault();
-      var val = $('#' + attr).val();
+    $('#' + attr).change(function (e) {
+      var val = $(this).val();
       $('[data-' + attr + ']').attr('data-' + attr, val);
     });
+  });
+
+  // prevent enter key from submitting form
+  $('input,select').keypress(function (e) {
+    return e.keyCode != 13;
   });
 
 }(jQuery));
